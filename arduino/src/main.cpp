f7eb5a5d136fe85196509a0f4d917f9c4602f3c5
Exaedro <owomolo123@gmail.com>
2024-11-12 21:29:06 -0300

    chau

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,28 +1,60 @@
 #include <Arduino.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Ethernet.h>
+#include <SPI.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };//Ponemos la dirección MAC de la Ethernet Shield
+IPAddress ip(192,168,1,177); //Asignamos  la IP al Arduino
+EthernetServer server(80);
 
 String dato;
 String tipo;
 String segundos;
 
 void setup() {
-  pinMode(2, OUTPUT);
   Serial.begin(9600);
+  Ethernet.begin(mac, ip);
+  server.begin();
+
+  pinMode(2, OUTPUT);
 }
 
 void loop() { 
-  while(Serial.available() > 0) {
-    dato = Serial.readString();
+  EthernetClient client = server.available();
 
-    tipo = dato.charAt(0);
-    segundos = dato.charAt(2);
-    int sec = segundos.toInt();
+  if(client) {
+    while(client.connected()) {
+      if(client.available()) {
+        char c = client.read();//Leemos la petición HTTP carácter por carácter
+        Serial.write(c);
+        while(Serial.available() > 0) {
+          
+          dato = Serial.readString();
 
-    if(tipo == "A") {
-      digitalWrite(2, HIGH);
-      _delay_ms(sec);
-      digitalWrite(2, LOW);
+          tipo = dato.charAt(0);
+          segundos = dato.charAt(2);
+
+          if(tipo == "A") {
+            digitalWrite(2, HIGH);
+            _delay_ms(1000);
+            digitalWrite(2, LOW);
+          }
+        }
+      }
     }
+
+    client.println("HTTP/1.1 200 OK");
+        client.println("Content-Type: text/html");
+        client.println();
+
+        //Página web en formato HTML
+        client.println("<html>");                 
+        client.println("<head><title>Naylamp Mechatronics</title>");
+        client.println("</head>");
+        client.println("<body>"); 
+        client.println(">");          
+        client.println("</body>");
+        client.println("</html>");
   }
 }
